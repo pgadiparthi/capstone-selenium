9d7991f45baf52d29fb441ef330afc71f2e8fb26
Prathap G <prathap@xyz.com>
2024-03-06 18:37:19 -0500

    modified pom.xml

--- src/main/java/insureme/capstone_selenium/App.java
+++ src/main/java/insureme/capstone_selenium/App.java
@@ -23,19 +23,19 @@ public class App
 		 
 		//System.setProperty("webdriver.chrome.driver", "C:\\Prathap\\Azure\\Bootcamp-TCS\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 	        
-	System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/google-chrome");
+	    //System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/google-chrome");
 		    
-		 //WebDriverManager.chromedriver().setup();
+		    WebDriverManager.chromedriver().setup();
 		    
 	        ChromeOptions chromeoptions = new ChromeOptions();
 	        chromeoptions.addArguments("--headless");
 	        
-	        chromeoptions.addArguments("start-maximized"); // open Browser in maximized mode
-	        chromeoptions.addArguments("disable-infobars"); // disabling infobars
-	        chromeoptions.addArguments("--disable-extensions"); // disabling extensions
-	        chromeoptions.addArguments("--disable-gpu"); // applicable to windows os only
-	        chromeoptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-	        chromeoptions.addArguments("--no-sandbox"); // Bypass OS security model
+	        //chromeoptions.addArguments("start-maximized"); // open Browser in maximized mode
+	        //chromeoptions.addArguments("disable-infobars"); // disabling infobars
+	        //chromeoptions.addArguments("--disable-extensions"); // disabling extensions
+	        //chromeoptions.addArguments("--disable-gpu"); // applicable to windows os only
+	        //chromeoptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+	        //chromeoptions.addArguments("--no-sandbox"); // Bypass OS security model
 	        //WebDriver driver = new ChromeDriver(options);
 	        
 	        WebDriver driver = new ChromeDriver(chromeoptions);
@@ -45,37 +45,37 @@ public class App
 	        
 	        //driver.get("http://107.21.193.163:8085/contact.html");
 	        
-	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	        
 	        
 	        //driver.findElement(By.className("nav-click")).click();
 	        
 	        //driver.findElement(By.cssSelector("a[id^='nav-click']")).click();
 	        
-	        Thread.sleep(1000);
+	        //Thread.sleep(1000);
 	        
-	        driver.findElement(By.id("inputName")).sendKeys("Prathap G");
+	       // driver.findElement(By.id("inputName")).sendKeys("Prathap G");
 	        
-	        Thread.sleep(1000);
+	        //Thread.sleep(1000);
 	        
-	        driver.findElement(By.id("inputNumber")).sendKeys("8889991111");
+	        //driver.findElement(By.id("inputNumber")).sendKeys("8889991111");
 	        
-	        Thread.sleep(1000);
+	        //Thread.sleep(1000);
 	        
-	        driver.findElement(By.id("inputMail")).sendKeys("admin@xyz.com");
+	        //driver.findElement(By.id("inputMail")).sendKeys("admin@xyz.com");
 	        
-	        Thread.sleep(1000);
+	        //Thread.sleep(1000);
 	        
-	        driver.findElement(By.id("inputMessage")).sendKeys("I am interested");
+	        //driver.findElement(By.id("inputMessage")).sendKeys("I am interested");
 	        
-	        Thread.sleep(1000);
+	        //Thread.sleep(1000);
 	        
 	        
-	        driver.findElement(By.id("my-button")).click();
+	        //driver.findElement(By.id("my-button")).click();
 	        
 	        //driver.findElement(By.className("btn-box")).click();
 	                          
-	       	Thread.sleep(3000);
+	       	//Thread.sleep(3000);
 	        
 	        System.out.println("Sent - Execution Completed");
 	        
